183bbbfb5019ae4d00f47eca9dde69a169f7a0b7
Darryl <darryljamescruz@gmail.com>
2025-03-04 01:59:57 -0800

    lab 18 working - able to navigate all pages from static site without any errors

--- packages/labs-server/dist/index.js
+++ packages/labs-server/dist/index.js
@@ -5,13 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
 const staticDir = process.env.STATIC_DIR || "public";
+console.log("STATIC_DIR:", process.env.STATIC_DIR);
+console.log("Serving file from:", path_1.default.resolve(staticDir, "index.html"));
 const app = (0, express_1.default)();
 app.use(express_1.default.static(staticDir));
 app.get("/hello", (req, res) => {
     res.send("Hello, World");
+});
+app.get("*", (req, res) => {
+    console.log("none of the routes above me were matched");
+    res.sendFile(path_1.default.resolve(staticDir, "index.html"));
 });
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
--- packages/labs-server/src/index.ts
+++ packages/labs-server/src/index.ts
@@ -1,15 +1,25 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
 const staticDir = process.env.STATIC_DIR || "public";
+
+console.log("STATIC_DIR:", process.env.STATIC_DIR);
+console.log("Serving file from:", path.resolve(staticDir, "index.html"));
+
 
 const app = express();
 app.use(express.static(staticDir));
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
+});
+
+app.get("*", (req: Request, res: Response) => {
+    console.log("none of the routes above me were matched");
+    res.sendFile(path.resolve(staticDir, "index.html"));
 });
 
 app.listen(PORT, () => {
