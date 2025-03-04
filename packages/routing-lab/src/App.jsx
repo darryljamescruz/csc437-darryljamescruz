e044b0281483738e5a4f1cdbd3ae6ba6ef07a115
Darryl <darryljamescruz@gmail.com>
2025-03-03 22:17:26 -0800

    added routing to navbar in App.jsx - so links route to other pages using react-router's browser router, routes

--- packages/routing-lab/src/App.jsx
+++ packages/routing-lab/src/App.jsx
@@ -2,16 +2,19 @@ import { Homepage } from "./Homepage";
 import { AccountSettings } from "./AccountSettings";
 import { ImageGallery } from "./images/ImageGallery.jsx";
 import { ImageDetails } from "./images/ImageDetails.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-    const POSSIBLE_PAGES = [
-        <Homepage userName="John Doe" />,
-        <AccountSettings />,
-        <ImageGallery />,
-        <ImageDetails imageId="0" />
-    ]
-
-    return POSSIBLE_PAGES[0];
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage userName="John Doe"/>} />
+                <Route path="/account" element={<AccountSettings/>} />
+                <Route path="/images" element={<ImageGallery/>} />
+                <Route path="/images/:imageId" element={<ImageDetails/>} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
