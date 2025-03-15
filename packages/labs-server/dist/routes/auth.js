b537347ae831f4853b8517f24fd465844a5bba50
Darryl <darryljamescruz@gmail.com>
2025-03-14 23:10:19 -0700

    Implement user registration with validation and password hashing

--- packages/labs-server/dist/routes/auth.js
+++ packages/labs-server/dist/routes/auth.js
@@ -1,9 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerAuthRoutes = registerAuthRoutes;
+const CredentialsProvider_1 = require("../CredentialsProvider");
 function registerAuthRoutes(app, mongoClient) {
     //handle post requests to /auth/register
     app.post("/auth/register", async (req, res) => {
-        res.send("register request received.");
+        const { username, password } = req.body;
+        console.log("Registering user: ", username);
+        console.log("password: ", password);
+        //check if username and password are provided
+        if (!username || !password) {
+            res.status(400).send({
+                error: "Bad request",
+                message: "Missing username or password"
+            });
+            return;
+        }
+        //create instance of credsprovider
+        const credsProvider = new CredentialsProvider_1.CredentialsProvider(mongoClient);
+        //attempt to register the user
+        const registrationSuccess = await credsProvider.registerUser(username, password);
+        //check if registration was successful
+        if (!registrationSuccess) {
+            res.status(400).send({
+                error: "Bad request",
+                message: "Usenamer already exists"
+            });
+            return;
+        }
+        //send a 201 response if registration was successful
+        res.status(201).send();
+        return;
     });
 }
--- packages/labs-server/src/routes/auth.ts
+++ packages/labs-server/src/routes/auth.ts
@@ -1,9 +1,39 @@
 import express, { Request, Response } from "express";
 import { MongoClient } from "mongodb";
+import { CredentialsProvider } from "../CredentialsProvider";
+
 
 export function registerAuthRoutes(app: express.Application, mongoClient: MongoClient) {
     //handle post requests to /auth/register
     app.post("/auth/register", async (req: Request, res: Response) => {
-        res.send("register request received.")
-    })
+        const { username, password } = req.body;
+        console.log("Registering user: ", username);
+        console.log("password: ", password);
+
+        //check if username and password are provided
+        if (!username || !password) {
+            res.status(400).send({
+                error: "Bad request",
+                message: "Missing username or password"
+            });
+            return;
+        }
+
+        //create instance of credsprovider
+        const credsProvider = new CredentialsProvider(mongoClient);
+        //attempt to register the user
+        const registrationSuccess = await credsProvider.registerUser(username, password);
+
+        //check if registration was successful
+        if (!registrationSuccess) {
+            res.status(400).send({
+                error: "Bad request",
+                message: "Usenamer already exists"
+            });
+            return;
+        }
+        //send a 201 response if registration was successful
+        res.status(201).send();
+        return;
+    });
 }
